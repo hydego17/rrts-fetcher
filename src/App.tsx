a904d21b3c2b924a194d4430aa1c6693c5867e49
Umma Ahimsha <uahimsha@gmail.com>
2020-12-10 22:59:11 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
+import styled from "styled-components";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <StyledApp>
       <h1>Test</h1>
-    </div>
+    </StyledApp>
   );
 }
 
-export default App;
+const StyledApp = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
